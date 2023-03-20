b032501201603e6a80cf0a3c42e93a3adc951e99
qseudo <57642143+yg-lim@users.noreply.github.com>
2023-03-20 17:30:56 +0900

    loads flight page with seed data

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const express = require("express");
 const PORT = 3000;
 const HOST = "localhost";
 const app = express();
+const flights = require("./lib/seed-data");
 
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("flights", { flights });
+});
+
+app.get("/add-flight", (req, res) => {
+  res.render("add-flight");
 });
 
 app.listen(PORT, HOST, () => {
